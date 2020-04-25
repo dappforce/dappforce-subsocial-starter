9962061cb5e5bba90ef940844f225fcb9221a9b8
Vladyslav Proshchavaiev <f3joule@gmail.com>
2020-04-25 19:07:55 +0300

    Change `update-ui-from-master` script
    Now it just pulls UI from docker hub, not builds it

--- scripts/update-ui-from-master.sh
+++ scripts/update-ui-from-master.sh
@@ -3,28 +3,19 @@
 set -e
 pushd . > /dev/null
 
-if [ -z $1 ]; then
-  bold=$(tput bold)
-  normal=$(tput sgr0)
-  echo "${bold}Usage:${normal} update-ui-from-master TAG [--global]"
+TAG=$(docker ps | grep subsocial-ui | cut -d ':' -f 2 | cut -d ' ' -f 1)
+if [ -z $TAG ]; then
+  printf '\033[0;31mWARN:\033[00m Web UI must be running to execute this script\n' >&2
   exit 1
-else
-  TAG=$1
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR\/..
 
-DIR=dappforce-subsocial-ui
+echo "Running start.sh with: --tag ${TAG} --global --only-webui --force-pull"
+eval ./start.sh --tag $TAG --global --only-webui --force-pull
 
-git clone --depth 1 https://github.com/dappforce/dappforce-subsocial-ui.git $DIR
-docker build --no-cache -f $DIR/docker/Dockerfile -t dappforce/subsocial-ui:$TAG $DIR
-
-IS_GLOBAL=[ ${2} = "--global"] && $2
-eval ./start.sh --tag $1 $IS_GLOBAL
-
-echo "Cleaning build cache and deprecated images..."
-rm -rf $DIR
+echo "Cleaning deprecated images..."
 eval docker system prune -f
 
 popd > /dev/null
