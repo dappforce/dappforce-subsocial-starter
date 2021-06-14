8294f8e58221084a6363908b4e98f1ef7b944b0f
Vladyslav Proshchavaiev <f3joule@gmail.com>
2021-06-14 14:00:14 +0300

    Add script to import keys into validator node

---
+++ scripts/insert-keys.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+pushd . > /dev/null
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+AURA_JSON=$DIR/keys/aura.json
+GRAN_JSON=$DIR/keys/gran.json
+
+curl http://localhost:9933 -H "Content-Type:application/json;charset=utf-8" -d "@$AURA_JSON"
+curl http://localhost:9933 -H "Content-Type:application/json;charset=utf-8" -d "@$GRAN_JSON"
+
+popd > /dev/null
+exit 0
