f531e3123cb0546dbb1c7107d396c92a056bf854
Alex Siman <aleksandr.siman@gmail.com>
2021-06-14 14:30:14 +0300

    Update insert-keys.sh

--- scripts/insert-keys.sh
+++ scripts/insert-keys.sh
@@ -7,8 +7,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 AURA_JSON=$DIR/keys/aura.json
 GRAN_JSON=$DIR/keys/gran.json
 
-curl http://localhost:9933 -H "Content-Type:application/json;charset=utf-8" -d "@$AURA_JSON"
-curl http://localhost:9933 -H "Content-Type:application/json;charset=utf-8" -d "@$GRAN_JSON"
+curl http://localhost:9934 -H "Content-Type:application/json;charset=utf-8" -d "@$AURA_JSON"
+curl http://localhost:9934 -H "Content-Type:application/json;charset=utf-8" -d "@$GRAN_JSON"
 
 popd > /dev/null
 exit 0
